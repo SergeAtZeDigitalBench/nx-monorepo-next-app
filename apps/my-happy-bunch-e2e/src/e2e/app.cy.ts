120b600ea53f66edeba8c30abdda3251c8c7be90
Serge <124575375+SergeAtZeDigitalBench@users.noreply.github.com>
2024-03-20 16:16:42 +0000

    fix: e2e test fixed, async pom locator

--- apps/my-happy-bunch-e2e/src/e2e/app.cy.ts
+++ apps/my-happy-bunch-e2e/src/e2e/app.cy.ts
@@ -3,11 +3,13 @@ import { getGreeting } from '../support/app.po';
 describe('my-happy-bunch-e2e', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
+  it('should display page heading message', () => {
     // Custom command example, see `../support/commands.ts` file
     cy.login('my-email@something.com', 'myPassword');
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome, please select from below/i);
+    // https://learn.cypress.io/cypress-fundamentals/understanding-the-asynchronous-nature-of-cypress
+    getGreeting().then(($heading) => {
+      cy.wrap($heading).contains(/Welcome, please select from below/i);
+    });
   });
 });
