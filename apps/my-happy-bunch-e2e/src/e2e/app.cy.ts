4b5aee5b5d989607a1468acb648de99ea98fe6bf
Serge <124575375+SergeAtZeDigitalBench@users.noreply.github.com>
2024-03-21 09:01:20 +0000

    fix: e2e wait

--- apps/my-happy-bunch-e2e/src/e2e/app.cy.ts
+++ apps/my-happy-bunch-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,15 @@
 import { getGreeting } from '../support/app.po';
 
 describe('my-happy-bunch-e2e', () => {
-  it('should display welcome message', () => {
-    cy.visit('/').then(() => {
-      // Custom command example, see `../support/commands.ts` file
-      cy.login('my-email@something.com', 'myPassword');
+  beforeEach(() => cy.visit('/'));
 
-      // Function helper example, see `../support/app.po.ts` file
-      getGreeting().contains(/Welcome, please select from below/i);
-    });
+  it('should display welcome message', () => {
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(3000);
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains(/Welcome, please select from below/i);
   });
 });
