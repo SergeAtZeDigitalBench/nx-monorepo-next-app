6f59886ff6743127a0b00916c1f20d09a41b68ac
Serge <124575375+SergeAtZeDigitalBench@users.noreply.github.com>
2024-03-21 09:19:53 +0000

    fix: cy visit ssr

--- apps/my-happy-bunch-e2e/src/e2e/app.cy.ts
+++ apps/my-happy-bunch-e2e/src/e2e/app.cy.ts
@@ -1,7 +1,14 @@
 import { getGreeting } from '../support/app.po';
 
 describe('my-happy-bunch-e2e', () => {
-  beforeEach(() => cy.visit('/', { failOnStatusCode: false }));
+  beforeEach(() =>
+    cy
+      .request('/')
+      .its('body')
+      .then((html) => {
+        cy.visit('/');
+      })
+  );
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
