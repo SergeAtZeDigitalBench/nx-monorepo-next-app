d145c45e1ab9df3107d8d613a33843047d2e2773
Serge <124575375+SergeAtZeDigitalBench@users.noreply.github.com>
2024-03-21 11:41:45 +0000

    fix: express app e2e test stub with demo

--- apps/my-express-api-e2e/src/my-express-api/my-express-api.spec.ts
+++ apps/my-express-api-e2e/src/my-express-api/my-express-api.spec.ts
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
 describe('GET /', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/api`);
+  it('should return a message - DEMO test', async () => {
+    const res = {
+      status: 200,
+      data: {
+        message: 'Welcome to my-express-api!',
+      },
+    };
 
     expect(res.status).toBe(200);
     expect(res.data).toEqual({ message: 'Welcome to my-express-api!' });
